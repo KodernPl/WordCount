084c35372311105de36fe8af7dd749b0df36a6c5
kodern <krzysztofkuziel84@gmail.com>
2018-09-04 21:20:30 +0200

    typo fix

--- WordCount.py
+++ WordCount.py
@@ -1,6 +1,6 @@
 # python 3.5
 # Word Counter
-# Find N most frequent word from a file 
+# Find N most frequent word from a file
 
 from collections import Counter
 import re
@@ -9,8 +9,8 @@ import os.path
 def find_and_print(input_file, top_frequent=20):
     """
     Method that open the file and find frequent words
-    @param input_file 
-    @param top_frequent 
+    @param input_file
+    @param top_frequent
     @return tuple top_frequent words
     """
     if top_frequent <= 0:
@@ -19,30 +19,34 @@ def find_and_print(input_file, top_frequent=20):
         # opening the file and divide it with regex
         words = re.findall(r'\w+', open(input_file).read().lower())
 
-        #most_common from Counter collection  counts n top frequent words 
-        wystepujace = Counter(words).most_common(top_frequent)
+        #most_common from Counter collection counts n top frequent words
+        existing_words = Counter(words).most_common(top_frequent)
     except:
         raise Exception("Error opening file: " + input_file) #raise opening file exception
 
-    return wystepujace
+    return existing_words
 
-#main
-while True:
-    file = input("Please enter a file name: ")
-    if(os.path.isfile(file)):
-        break
-    print(file + " Does not exist. Try again." )
+def main():
+    while True:
+        file = input("Please enter a file name: ")
+        if(os.path.isfile(file)):
+            break
+        print(file + " Does not exist. Try again." )
 
-try:
-    top_frequent = int(input("Enter number of top words to find: "))
-    wyrazy = find_and_print(file, top_frequent)
-    print("Top " + str(top_frequent) + " frequent words in " + file + " are:")
-    print(wyrazy)
+    try:
+        top_frequent = int(input("Enter number of top words to find: "))
+        words = find_and_print(file, top_frequent)
+        print("Top " + str(top_frequent) + " frequent words in " + file + " are:")
+        print(words)
 
-except Exception as arg:
-    print(arg)
+    except Exception as arg:
+        print(arg)
 
-try:
-	int(input("Press Enter key to continue..."))
-except:
-	pass
+    try:
+        int(input("Press Enter key to continue..."))
+    except:
+        pass
+
+if __name__ == '__main__':
+    main()
+
